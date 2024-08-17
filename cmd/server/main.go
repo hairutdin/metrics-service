fdd0e34effbd65700443698d7997d5a89c86cb68
hairutdin <hairutdinaliev@gmail.com>
2024-08-17 13:44:53 +0300

    Initial commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,87 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type MemStorage struct {
+	sync.RWMutex
+	Gauges   map[string]float64
+	Counters map[string]int64
+}
+
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		Gauges:   make(map[string]float64),
+		Counters: make(map[string]int64),
+	}
+}
+
+func (s *MemStorage) UpdateGauge(name string, value float64) {
+	s.Lock()
+	defer s.Unlock()
+	s.Gauges[name] = value
+}
+
+func (s *MemStorage) UpdateCounter(name string, value int64) {
+	s.Lock()
+	defer s.Unlock()
+	s.Counters[name] += value
+}
+
+func main() {
+	storage := NewMemStorage()
+
+	http.HandleFunc("/update/", func(w http.ResponseWriter, r *http.Request) {
+		handleUpdate(storage, w, r)
+	})
+
+	fmt.Println("Server is running at http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Server failed to start: %v\n", err)
+	}
+}
+
+func handleUpdate(storage *MemStorage, w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(strings.TrimPrefix(r.URL.Path, "/update/"), "/")
+	if len(parts) != 3 {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	metricType := parts[0]
+	metricName := parts[1]
+	metricValue := parts[2]
+
+	if metricName == "" {
+		http.Error(w, "Metric name is required", http.StatusNotFound)
+		return
+	}
+
+	switch metricType {
+	case "gauge":
+		value, err := strconv.ParseFloat(metricValue, 64)
+		if err != nil {
+			http.Error(w, "Invalid gauge value", http.StatusBadRequest)
+			return
+		}
+		storage.UpdateGauge(metricName, value)
+	case "counter":
+		value, err := strconv.ParseInt(metricValue, 10, 64)
+		if err != nil {
+			http.Error(w, "Invalid counter value", http.StatusBadRequest)
+			return
+		}
+		storage.UpdateCounter(metricName, value)
+	default:
+		http.Error(w, "Invalid metric type", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
