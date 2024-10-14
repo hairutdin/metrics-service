6006827e51f0b875c8573eeb3b73d52e13483c3b
hairutdin <hairutdinaliev@gmail.com>
2024-10-14 21:19:50 +0300

    Add periodic metric storage, restoration

--- storage/mem_storage.go
+++ storage/mem_storage.go
@@ -1,8 +1,11 @@
 package storage
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"sync"
+	"time"
 )
 
 type MemStorage struct {
@@ -61,4 +64,65 @@ func (s *MemStorage) GetAllMetrics() map[string]string {
 		metrics[name] = fmt.Sprintf("counter: %d", value)
 	}
 	return metrics
+}
+
+func (s *MemStorage) SaveMetricsToFile(filePath string) error {
+	s.RLock()
+	defer s.RUnlock()
+
+	data := struct {
+		Gauges   map[string]float64 `json:"gauges"`
+		Counters map[string]int64   `json:"counters"`
+	}{
+		Gauges:   s.Gauges,
+		Counters: s.Counters,
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("error creating file: %v", err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(data); err != nil {
+		return fmt.Errorf("error encoding metrics to file: %v", err)
+	}
+
+	return nil
+}
+
+func (s *MemStorage) RestoreMetricsFromFile(filePath string) error {
+	s.Lock()
+	defer s.Unlock()
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	data := struct {
+		Gauges   map[string]float64 `json:"gauges"`
+		Counters map[string]int64   `json:"counters"`
+	}{}
+
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&data); err != nil {
+		return fmt.Errorf("error decoding metrics from file: %v", err)
+	}
+
+	s.Gauges = data.Gauges
+	s.Counters = data.Counters
+
+	return nil
+}
+
+func (s *MemStorage) EnableSyncSaving(filePath string) {
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			s.SaveMetricsToFile(filePath)
+		}
+	}()
 }
